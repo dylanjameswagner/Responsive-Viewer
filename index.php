cd9535c6da3a58e6235a7d5654045dae1a69996d
Dylan James Wagner <hello@dylanjameswagner.com>
2015-11-18 00:32:05 -0500

    refactor scripts and styles

--- index.php
+++ index.php
@@ -1,204 +1,76 @@
-<?php
-	// $frames[] = array(array(240,320)	,'stretch');
-	// $frames[] = array(array(320,320)	,'stretch');
-	// $frames[] = array(array(360,320)	,'stretch');
-	// $frames[] = array(array(375,320)	,'stretch');
-	// $frames[] = array(array(414,320)	,'stretch');
-	// $frames[] = array(array(480,320)	,'stretch');
-	// $frames[] = array(array(568,320)	,'stretch');
-	// $frames[] = array(array(600,320)	,'stretch');
-	// $frames[] = array(array(640,320)	,'stretch');
-	// $frames[] = array(array(667,320)	,'stretch');
-	// $frames[] = array(array(768,320)	,'stretch');
-	// $frames[] = array(array(1024,320)	,'stretch');
-	// $frames[] = array(array(1280,320)	,'stretch');
-	// $frames[] = array(array(1440,320)	,'stretch');
-	// $frames[] = array(array(1600,320)	,'stretch');
-	// $frames[] = array(array(1680,320)	,'stretch');
-
-	// $frames[] = array(array(1280,800)	,'device'	,'MacBook Pro 13');			// non-retina
-	// $frames[] = array(array(1440,900)	,'device'	,'MacBook Pro 15');			// non-retina
-	// $frames[] = array(array(1680,1050)	,'device'	,'MacBook Pro 15 Hi-Res');	// non-retina
-
-	// $frames[] = array(array(600,1024)	,'device'	,'Kindle Fire');
-	// $frames[] = array(array(768,1024)	,'device'	,'iPad');
-	// $frames[] = array(array(1024,768)	,'device'	,'iPad');
-
-	// // $frames[] = array(array(360,640)	,'device'	,'iPhone 6 Plus');			// @3x 1920 × 1080 // INCORRECT ASSUMPTION based on 1080 × 1920
-
-	// // $frames[] = array(array(375,667)	,'device'	,'iPhone 6');				// @2x 750 × 1334
-	// $frames[] = array(array(414,736)	,'device'	,'iPhone 6 Plus');			// @3x 1242 × 2208 downsampling / 1.15 (* .8695652174) to 1080 × 1920
-
-	// // $frames[] = array(array(320,568)	,'device'	,'iPhone 6 Zoom');			// @2x 750 × 1334 upsampling * 1.171875 = 750 × 1334
-	// $frames[] = array(array(375,667)	,'device'	,'iPhone 6/6 Plus Zoom');	// @2x 1125 × 2001 downsampling * 0.96 = 1080 × 1920
-
-	// $frames[] = array(array(360,640)	,'device'	,'Galaxy S4');				// @3x 1920 × 1080
-
-	// $frames[] = array(array(320,568)	,'device'	,'iPhone 5/6 Zoom'); 		// @2x 1136 × 640
-	// $frames[] = array(array(320,480)	,'device'	,'iPhone');					// @2x 640 × 960
-
-	// $frames[] = array(array(320,372)	,'viewport'		,'iPhone'		,'iOS 7 Safari');
-	// $frames[] = array(array(480,212)	,'viewport min'	,'iPhone'		,'iOS 7 Safari Min');
-	// $frames[] = array(array(480,320)	,'viewport max'	,'iPhone'		,'iOS 7 Safari Max');
-
-	// $frames[] = array(array(568,232)	,'viewport min' ,'iPhone 5'		,'iOS 8 Safari Min');
-	// $frames[] = array(array(568,320)	,'viewport max'	,'iPhone 5'		,'iOS 8 Safari Max');
-	// $frames[] = array(array(320,460)	,'viewport min'	,'iPhone 5'		,'iOS 8 Safari Min');
-	// $frames[] = array(array(320,529)	,'viewport max'	,'iPhone 5'		,'iOS 8 Safari Max');
-
-	// // $frames[] = array(array(320,460)	,'viewport min'	,'iPhone 5'		,'iOS 7 Safari Min');
-	// // $frames[] = array(array(320,583)	,'viewport max'	,'iPhone 5'		,'iOS 7 Safari Max');
-	// // $frames[] = array(array(568,212)	,'viewport min' ,'iPhone 5'		,'iOS 7 Safari Min');
-	// // $frames[] = array(array(568,320)	,'viewport max'	,'iPhone 5'		,'iOS 7 Safari Max');
-
-	// $frames[] = array(array(667,331)	,'viewport min'	,'iPhone 6'		,'iOS 8 Safari Min');
-	// $frames[] = array(array(667,375)	,'viewport max'	,'iPhone 6'		,'iOS 8 Safari Max');
-	// $frames[] = array(array(375,559)	,'viewport min'	,'iPhone 6'		,'iOS 8 Safari Min');
-	// $frames[] = array(array(375,628)	,'viewport max'	,'iPhone 6'		,'iOS 8 Safari Max');
-
-	// // $frames[] = array(array(360,559)	,'viewport'		,'iPhone 6 Plus','iOS 8 Safari');
-	// // $frames[] = array(array(667,331)	,'viewport'		,'iPhone 6 Plus','iOS 8 Safari');
-
-	// $frames[] = array(array(768,928)	,'viewport'		,'iPad Mini'	,'iOS 8 Safari');
-	// $frames[] = array(array(1024,672)	,'viewport'		,'iPad Mini'	,'iOS 8 Safari');
-
-	// $frames[] = array(array(768,928)	,'viewport'		,'iPad'			,'iOS 8 Safari');
-	// $frames[] = array(array(1024,672)	,'viewport'		,'iPad'			,'iOS 8 Safari');
-
-	// $frames[] = array(array(1538,815)	,'viewport'		,'desktop');			// cesley's chrome
-
-	// // $frames = array_reverse($frames);
-
-	$stretch[] = array(array(240,'100%'));
-	$stretch[] = array(array(320,'100%'));
-	$stretch[] = array(array(360,'100%'));
-	$stretch[] = array(array(375,'100%'));
-	$stretch[] = array(array(414,'100%'));
-	$stretch[] = array(array(480,'100%'));
-	$stretch[] = array(array(568,'100%'));
-	$stretch[] = array(array(600,'100%'));
-	$stretch[] = array(array(640,'100%'));
-	$stretch[] = array(array(667,'100%'));
-	$stretch[] = array(array(768,'100%'));
-	$stretch[] = array(array(1024,'100%'));
-	$stretch[] = array(array(1280,'100%'));
-	$stretch[] = array(array(1440,'100%'));
-	$stretch[] = array(array(1600,'100%'));
-	$stretch[] = array(array(1680,'100%'));
-
-	$device[] = array(array(240,320),'Mobile');
-	$device[] = array(array(320,480),'iPhone 4');
-	$device[] = array(array(320,568),'iPhone 5/6 Zoom'); // @2x 640 × 1136
-	$device[] = array(array(360,640),'Galaxy S3/4/5/6');
-	$device[] = array(array(375,667),'iPhone 6/6 Plus Zoom'); // @2x 1125 × 2001 downsampling * 0.96 = 1080 × 1920
-	$device[] = array(array(414,736),'iPhone 6 Plus'); // @3x 1242 × 2208 downsampling / 1.15 (* .8695652174) to 1080 × 1920
-	$device[] = array(array(480,320),'iPhone 4');
-	$device[] = array(array(568,320),'iPhone 5/6 Zoom');
-	$device[] = array(array(600,1024),'Kindle Fire');
-	$device[] = array(array(640,360),'Galaxy S3/4/5/6');
-	$device[] = array(array(667,375),'iPhone 6/6 Plus Zoom');
-	$device[] = array(array(768,1024),'iPad');
-	$device[] = array(array(1024,768),'iPad');
-	$device[] = array(array(1280,800),'MacBook Pro 13'); // non-retina
-	$device[] = array(array(1440,900),'MacBook Pro 15'); // non-retina
-	$device[] = array(array(1600,1200),'Desktop');
-	$device[] = array(array(1680,1050),'MacBook Pro 15 Hi-Res'); // non-retina
-
-	$viewport[] = array(array(320,372),'iPhone 4','iOS 7 Safari');
-	$viewport[] = array(array(480,212),'iPhone 4','iOS 7 Safari Min');
-	$viewport[] = array(array(480,320),'iPhone 4','iOS 7 Safari Max');
-
-	$viewport[] = array(array(360,559),'Galaxy S6','Android 4.4 Chrome');
-	$viewport[] = array(array(640,279),'Galaxy S6',' Android 4.4 Chrome');
-
-	$viewport[] = array(array(320,460),'iPhone 5','iOS 8 Safari Min');
-	$viewport[] = array(array(320,529),'iPhone 5','iOS 8 Safari Max');
-	$viewport[] = array(array(568,232),'iPhone 5','iOS 8 Safari Min');
-	$viewport[] = array(array(568,320),'iPhone 5','iOS 8 Safari Max');
-
-	$viewport[] = array(array(375,559),'iPhone 6','iOS 8 Safari Min');
-	$viewport[] = array(array(375,628),'iPhone 6','iOS 8 Safari Max');
-	$viewport[] = array(array(667,331),'iPhone 6','iOS 8 Safari Min');
-	$viewport[] = array(array(667,375),'iPhone 6','iOS 8 Safari Max');
-
-	$viewport[] = array(array(768,928),'iPad','iOS 8 Safari');
-	$viewport[] = array(array(1024,672),'iPad','iOS 8 Safari');
-?>
 <!doctype html>
 <html>
 <head>
 
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-	<meta name="viewport" content="width=device-width,minimum-scale=0.5,maximum-scale=1.0,user-scalable=no"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+    <meta name="viewport" content="width=device-width, minimum-scale=0.5, maximum-scale=1.0, user-scalable=no"/>
 
-	<title>responsive</title>
+    <title>responsive</title>
 
-	<link rel="shortcut icon" type="image/x-icon" href="./public/images/favicon.ico"/>
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="./public/images/apple-touch-icon.png"/>
+    <link rel="shortcut icon" type="image/x-icon" href="./public/images/favicon.ico"/>
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./public/images/apple-touch-icon.png"/>
 
 <!--[if gte IE 9]><!-->
 
-	<link rel="stylesheet prefetch" href="http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic"/>
-	<link rel="stylesheet" href="./public/styles/custom.css"/>
+    <link rel="stylesheet prefetch" href="http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic"/>
+    <link rel="stylesheet" href="./public/styles/custom.css"/>
 
-	<script src="./public/scripts/html5shiv.printshiv-3.6.2.min.js"></script>
-	<script src="./public/scripts/prefixfree-1.0.7.min.js"></script>
+    <script src="./public/scripts/prefixfree-1.0.7.min.js"></script>
 
 <!--<![endif]-->
 
 </head>
 <body>
 
-	<header id="header">
-		<form id="header-form" method="post">
-			<p id="header-form-url"><input type="text" placeholder="enter a url"/> <button>Reload All</button></p>
-			<p id="header-form-settings">
-				<label><input type="radio"		id="stretch"	name="view"		value="stretch" checked="checked"/>	Stretch</label>
-				<label><input type="radio"		id="device"		name="view"		value="device"/>					Device</label>
-				<label><input type="radio"		id="viewport"	name="view"		value="viewport"/>					Viewport</label>
-				<label><input type="checkbox"	id="reverse"	name="reverse"	value="reverse"/>					Reverse</label>
-			</p>
-		</form>
+    <header id="header">
+        <form id="header-form" method="post">
+            <p id="header-form-url"><input type="text" placeholder="enter a url"/> <button>Reload All</button></p>
+            <p id="header-form-settings">
+                <label><input type="radio"      id="stretch"    name="view"     value="stretch" checked="checked"/> Stretch</label>
+                <label><input type="radio"      id="device"     name="view"     value="device"/>                    Device</label>
+                <label><input type="radio"      id="viewport"   name="view"     value="viewport"/>                  Viewport</label>
+                <label><input type="checkbox"   id="reverse"    name="reverse"  value="reverse"/>                   Reverse</label>
+            </p>
+        </form>
 
-		<p id="header-credit">A tool based on the work of <a target="_blank" href="http://mattkersley.com">Matt Kersley</a> &raquo; <a target="_blank" href="https://github.com/mattkersley/Responsive-Design-Testing">Github</a></p>
-	</header >
+        <p id="header-credit">A tool based on the work of <a target="_blank" href="http://mattkersley.com">Matt Kersley</a> &raquo; <a target="_blank" href="https://github.com/mattkersley/Responsive-Design-Testing">Github</a></p>
+    </header >
 
-	<section id="frames" class="stretch">
-		<div id="frames-inner">
+    <section id="frames" class="stretch">
+        <div id="frames-inner">
 
 <?php /* function frame($args){ ?>
 
-			<div class="frame <?php echo $args[1]; ?>">
-				<h2 class="frame-title"><span class="meta-width"> <?php echo $args[0][0]; ?></span> <span class="meta-delimiter">×</span> <span class="meta-height"><?php echo $args[0][1]; ?></span><?php if ($args[2]): ?> <small><?php echo $args[2]; ?> <?php if ($args[1] == 'viewport'): ?><span class="meta-delimiter">-</span> <?php echo $args[3]; ?><?php endif; // $args[1] == viewport ?></small><?php endif; // $args[2] ?> <button>Reload</button></h2>
-				<iframe sandbox="allow-same-origin allow-forms allow-scripts" width="<?php echo $args[0][0]; ?>" height="<?php echo $args[0][1]; ?>" seamless></iframe>
-			</div>
+            <div class="frame <?php echo $args[1]; ?>">
+                <h2 class="frame-title"><span class="meta-width"> <?php echo $args[0][0]; ?></span> <span class="meta-delimiter">×</span> <span class="meta-height"><?php echo $args[0][1]; ?></span><?php if ($args[2]): ?> <small><?php echo $args[2]; ?> <?php if ($args[1] == 'viewport'): ?><span class="meta-delimiter">-</span> <?php echo $args[3]; ?><?php endif; // $args[1] == viewport ?></small><?php endif; // $args[2] ?> <button>Reload</button></h2>
+                <iframe sandbox="allow-same-origin allow-forms allow-scripts" width="<?php echo $args[0][0]; ?>" height="<?php echo $args[0][1]; ?>" seamless></iframe>
+            </div>
 
 <?php } // frame ?>
 <?php foreach ($frames as $frame): frame($frame); endforeach; */ ?>
 
-<?php function iframe($iframe,$class){ ?>
+<?php function iframe($iframe, $class){ ?>
 
-			<div class="frame <?php echo $class; ?>">
-				<h2 class="frame-title"><span class="meta-width"> <?php echo $iframe[0][0]; ?></span> <span class="meta-delimiter">×</span> <span class="meta-height"><?php echo $iframe[0][1]; ?></span><?php if ($class != 'stretch' && $iframe[1]): ?> <small><?php echo $iframe[1]; ?> <?php if ($class == 'viewport'): ?><span class="meta-delimiter">-</span> <?php echo $iframe[2]; ?><?php endif; // $class == viewport ?></small><?php endif; // $iframe[1] ?> <button>Reload</button></h2>
-				<iframe sandbox="allow-same-origin allow-forms allow-scripts" width="<?php echo $iframe[0][0]; ?>" height="<?php echo $iframe[0][1]; ?>" seamless></iframe>
-			</div>
+            <div class="frame <?php echo $class; ?>">
+                <h2 class="frame-title"><span class="meta-width"> <?php echo $iframe[0][0]; ?></span> <span class="meta-delimiter">×</span> <span class="meta-height"><?php echo $iframe[0][1]; ?></span><?php if ($class != 'stretch' && $iframe[1]): ?> <small><?php echo $iframe[1]; ?> <?php if ($class == 'viewport'): ?><span class="meta-delimiter">-</span> <?php echo $iframe[2]; ?><?php endif; // $class == viewport ?></small><?php endif; // $iframe[1] ?> <button>Reload</button></h2>
+                <iframe sandbox="allow-same-origin allow-forms allow-scripts" width="<?php echo $iframe[0][0]; ?>" height="<?php echo $iframe[0][1]; ?>" seamless></iframe>
+            </div>
 
 <?php } // frame ?>
 
-<?php foreach ($stretch as $iframe): iframe($iframe,'stretch'); endforeach; ?>
-<?php foreach ($device as $iframe): iframe($iframe,'device'); endforeach; ?>
-<?php foreach ($viewport as $iframe): iframe($iframe,'viewport'); endforeach; ?>
+<?php foreach ($stretch as $iframe): iframe($iframe, 'stretch'); endforeach; ?>
+<?php foreach ($device as $iframe): iframe($iframe, 'device'); endforeach; ?>
+<?php foreach ($viewport as $iframe): iframe($iframe, 'viewport'); endforeach; ?>
 
-		</div>
-	</section>
+        </div>
+    </section>
 
 <!--[if gte IE 9]><!-->
 
-	<!--script src="./public/scripts/jquery-1.11.0.min.js"></script-->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="./public/scripts/base.js"></script>
-	<script src="./public/scripts/custom.js"></script>
+    <!--script src="./public/scripts/jquery-1.11.0.min.js"></script-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="./public/scripts/custom.js"></script>
 
 <!--<![endif]-->
 
--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,28 +1,26 @@
-/* custom.js */
+jQuery(document).ready(function($) {
 
-jQuery(document).ready(function($){
+	if (cookieRead('view')	  != 0) { $('input[value="'+cookieRead('view')+'"').attr('checked', 'checked'); }
+	if (cookieRead('reverse') != 0) { $('input[value="reverse"]').attr('checked', 'checked'); framesReverse(); }
 
-	if (cookieRead('view')	  != 0){ $('input[value="'+cookieRead('view')+'"').attr('checked','checked'); }
-	if (cookieRead('reverse') != 0){ $('input[value="reverse"]').attr('checked','checked'); framesReverse(); }
+	$(window).on('load resize', function() { framesWidth(); framesHeight(); });
 
-	$(window).on('load resize',function(){ framesWidth(); framesHeight(); });
+	$('#header-form').submit(function(event) { event.preventDefault();
+		if (!confirm('Reload All Frames?')) { event.preventDefault(); return; }
 
-	$('#header-form').submit(function(event){ event.preventDefault();
-		if (!confirm('Reload All Frames?')){ event.preventDefault(); return; }
-
-		window.location = '?'+$('#header-form-url input').val(); //.replace('https:','').replace('https:','');
+		window.location = '?'+$('#header-form-url input').val(); //.replace('https:', '').replace('https:', '');
 	});
 
 	framesLoad(window.location.href.split('?')[1]);
 
 	$('#header-form-settings input[type="checkbox"]')
-		.on('change',function(){
+		.on('change', function() {
 			framesReverse();
 		});
 
 	$('#header-form-settings input[type="radio"]')
-		.on('change',function(){
-			$('#frames').attr('class','').addClass($(this).val());
+		.on('change', function() {
+			$('#frames').attr('class', '').addClass($(this).val());
 			$('#frames .frame').hide();
 			$('#frames .'+$(this).val()).show();
 
@@ -30,32 +28,32 @@ jQuery(document).ready(function($){
 		});
 
 	$('.frame button')
-		.on('click',function(){
+		.on('click', function() {
 			var iframe = $(this).closest('.frame').find('iframe');
-				iframe.attr('src',iframe.attr('src'));
+				iframe.attr('src', iframe.attr('src'));
 		});
 
 }); // .ready
 
-	function framesLoad(url){
-		if (!url){ url = 'example.com'; }
+	function framesLoad(url) {
+		if (!url) { url = 'example.com'; }
 
-		if ($('input[type="radio"]:checked').length){ cookieWrite('view',$('input[type="radio"]:checked').val()); }
-		else										{ cookieWrite('view',0); }
+		if ($('input[type="radio"]:checked').length) { cookieWrite('view', $('input[type="radio"]:checked').val()); }
+		else										{ cookieWrite('view', 0); }
 
 		$('.frame').hide();
 		$('.frame.'+cookieRead('view')).show();
 
 		$('#header-form-url input').val(url);
-		$('.frame:visible iframe').addClass('loaded').attr('src',(url.substr(0,7) != 'http://' && url.substr(0,8) != 'https://' && url.substr(0,2) != '//' ? '//' : '') + url);
+		$('.frame:visible iframe').addClass('loaded').attr('src', (url.substr(0, 7) != 'http://' && url.substr(0, 8) != 'https://' && url.substr(0, 2) != '//' ? '//' : '') + url);
 	}
 
-	function framesWidth(){
-		$('#frames-inner').css('width',function(){
+	function framesWidth() {
+		$('#frames-inner').css('width', function() {
 			var padding = 18;
 			var width = padding;
 
-			$('#frames .frame:visible').each(function(){
+			$('#frames .frame:visible').each(function() {
 				width += $(this).outerWidth() + padding;
 // console.log(width);
 			});
@@ -64,19 +62,60 @@ jQuery(document).ready(function($){
 		});
 	}
 
-	function framesHeight(){
+	function framesHeight() {
 		var height = $(window).innerHeight() - $('#header').outerHeight();
 console.log(height - (parseInt($('#frames-inner').css('padding-top')) * 2));
 		$('#frames').height(height);
 		$('.frame.stretch iframe').height(height - (parseInt($('#frames-inner').css('padding-top')) * 2) - 39);
 	}
 
-	function framesReverse(){
-		if ($('input[type="checkbox"]:checked').length)	{ cookieWrite('reverse',1); }
-		else											{ cookieWrite('reverse',0); }
+	function framesReverse() {
+		if ($('input[type="checkbox"]:checked').length)	{ cookieWrite('reverse', 1); }
+		else											{ cookieWrite('reverse', 0); }
 
 		$('#frames .frame')
-			.each(function(index,element){
+			.each(function(index, element) {
 				$(this).prependTo('#frames-inner');
 			});
+	}
+
+/* cookies */
+
+	function cookieWrite(name, value, days) {
+		// var date = new Date();
+		// var expire = date.setMonth(expireDate.getMonth()+6);
+		// document.cookie = name+'='+date+';expires='+expire.toGMTString();
+		// return null;
+		var expires = '';
+		if (days) {
+			var date = new Date();
+				date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = '; expires=' + date.toGMTString();
+		}
+		else {
+			expires = '';
+		}
+		document.cookie = name + '=' + value + expires + '; path=/';
+	}
+
+	function cookieRead(name) {
+		// var thisCookie = document.cookie.split('; ');
+		// for (var i=0; i<thisCookie.length; i++) {
+		// 	if (name == thisCookie[i].split('=')[0]) {
+		// 		return thisCookie[i].split('=')[1];
+		// 	}
+		// }
+		// return false;
+		var nameEQ = name + '=';
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+		}
+		return null;
+	}
+
+	function cookieDelete(name) {
+		cookieWrite(name, '', -1);
 	}
