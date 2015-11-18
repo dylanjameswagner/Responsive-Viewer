3d003ee796b9f881b35b1fffbc91dae92b3f7cf3
Dylan James Wagner <hello@dylanjameswagner.com>
2015-11-18 00:47:44 -0500

    add CDN fallbacks for jQuery and PrefixFree

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
     <link rel="stylesheet prefetch" href="http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic"/>
     <link rel="stylesheet" href="./public/styles/custom.css"/>
 
-    <script src="./public/scripts/prefixfree-1.0.7.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script><script>if (typeof PrefixFree == 'undefined') { document.write(unescape('%3Cscript src="./public/scripts/prefixfree-1.0.7.min.js"%3E%3C/script%3E')); }</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script><script>if (typeof jQuery == 'undefined') { document.write(unescape('%3Cscript src="./public/scripts/jquery-2.1.4.min.js"%3E%3C/script%3E')); }</script>
+    <script src="./public/scripts/custom.js"></script>
 
 <!--<![endif]-->
 
@@ -43,7 +45,7 @@
 <?php /* function frame($args){ ?>
 
             <div class="frame <?php echo $args[1]; ?>">
-                <h2 class="frame-title"><span class="meta-width"> <?php echo $args[0][0]; ?></span> <span class="meta-delimiter">×</span> <span class="meta-height"><?php echo $args[0][1]; ?></span><?php if ($args[2]): ?> <small><?php echo $args[2]; ?> <?php if ($args[1] == 'viewport'): ?><span class="meta-delimiter">-</span> <?php echo $args[3]; ?><?php endif; // $args[1] == viewport ?></small><?php endif; // $args[2] ?> <button>Reload</button></h2>
+                <h2 class="frame-title"><span class="meta-width"> <?php echo $args[0][0]; ?></span> <span class="meta-delimiter">×</span> <span class="meta-height"><?php echo $args[0][1]; ?></span><?php if ($args[2]): ?> <small><?php echo $args[2]; ?> <?php if ($args[1] == "viewport'): ?><span class="meta-delimiter">-</span> <?php echo $args[3]; ?><?php endif; // $args[1] == viewport ?></small><?php endif; // $args[2] ?> <button>Reload</button></h2>
                 <iframe sandbox="allow-same-origin allow-forms allow-scripts" width="<?php echo $args[0][0]; ?>" height="<?php echo $args[0][1]; ?>" seamless></iframe>
             </div>
 
@@ -68,9 +70,6 @@
 
 <!--[if gte IE 9]><!-->
 
-    <!--script src="./public/scripts/jquery-1.11.0.min.js"></script-->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="./public/scripts/custom.js"></script>
 
 <!--<![endif]-->
 
